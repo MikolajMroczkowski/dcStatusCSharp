f60e2c4a9d0f1fb0e9e518a7311eedaefe3e0454
MikolajMroczkowski <mikolaj.mroczkowski@gmail.com>
2022-01-10 10:08:56 +0100

    Initial

---
+++ dcStatus/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace dcStatus
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            DataPatern status = new DataPatern();
+        }
+
+        public static string ChangeStatus(string token, fullDataPatern statusData)
+        {
+            var url = "https://discord.com/api/v9/users/@me/settings";
+
+            var httpRequest = (HttpWebRequest)WebRequest.Create(url);
+            httpRequest.Method = "PATCH";
+
+            httpRequest.ContentType = "application/json";
+            httpRequest.Headers["authorization"] = token;
+            var data = JsonConvert.SerializeObject(statusData);
+
+            using (var streamWriter = new StreamWriter(httpRequest.GetRequestStream()))
+            {
+                streamWriter.Write(data);
+            }
+
+            var httpResponse = (HttpWebResponse)httpRequest.GetResponse();
+            using (var streamReader = new StreamReader(httpResponse.GetResponseStream()))
+            {
+                var result = streamReader.ReadToEnd();
+            }
+
+            return httpResponse.StatusCode.ToString();
+        }
+        
+    }
+
+    class fullDataPatern
+    {
+        public List<DataPatern> custom_status;
+    }
+    class DataPatern
+    {
+        public string emoji;
+        public string status;
+
+    }
+}
